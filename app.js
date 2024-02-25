7c37bb53c8ad53a12ed9fccb06db6f102fe72072
Hermano Uchôa <hermanouchoa@gmail.com>
2024-02-24 21:29:58 -0300

    removendo linhar comentadas

--- app.js
+++ app.js
@@ -1,14 +1,5 @@
 var app = require('./config/server');
 
-//var rotaHome = require('./app/routes/home')
-//rotaHome(app);
-
-//var rotaNoticias = require('./app/routes/noticias');
-//rotaNoticias(app);
-
-//var rotaInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')
-//rotaInclusaoNoticias(app);
-
 app.listen(3000, function (req, res) {
     console.log("Jesus me abençoe nessa jornada");
 });
