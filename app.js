ddbe5405e5fbb6fec005379f9e3a87446bdedeef
Hermano Uchôa <hermanouchoa@gmail.com>
2024-02-24 21:29:24 -0300

    utilizando consign para mapear rotas

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var app = require('./config/server');
 
-var rotaHome = require('./app/routes/home')
-rotaHome(app);
+//var rotaHome = require('./app/routes/home')
+//rotaHome(app);
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+//var rotaNoticias = require('./app/routes/noticias');
+//rotaNoticias(app);
 
-var rotaInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')
-rotaInclusaoNoticias(app);
+//var rotaInclusaoNoticias = require('./app/routes/formulario_inclusao_noticia')
+//rotaInclusaoNoticias(app);
 
 app.listen(3000, function (req, res) {
     console.log("Jesus me abençoe nessa jornada");
