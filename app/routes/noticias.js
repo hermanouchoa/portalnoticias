c0ea53cd5836909f8992dd2c00b37fb73993b140
Hermano Uchoa <hermano.uchoa@icc.org.br>
2024-02-27 19:25:03 -0300

    refatorando conexão com banco. Criando classe NoticiasDAO

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -3,9 +3,9 @@ module.exports = function (applicantion) {
     applicantion.get('/noticias', function (req, res) {    
 
         var connection = applicantion.config.dbConnection();
-        var noticiasModel = applicantion.app.models.noticiasModel;
+        var noticiasModel = new applicantion.app.models.NoticiasDAO(connection);
 
-        noticiasModel.getNoticias(connection, function (error, result) {
+        noticiasModel.getNoticias(function (error, result) {
             res.render("noticias/noticias", 
             {
                 noticias: result
