21776fe7a1e0ba54db8ad40fa4055b8c4774597e
Hermano Uchôa <hermanouchoa@gmail.com>
2024-03-03 11:13:42 -0300

    tratando dados do formulario de login

--- app/controllers/seguranca.js
+++ app/controllers/seguranca.js
@@ -1,9 +1,19 @@
 module.exports.login = function (application, req, res) {
-    res.render("seguranca/login");
+    res.render("seguranca/login", {validacao: {}});
 }
 
 module.exports.autenticar = function (application, req, res) {
     var dadosForm = req.body;
-    // terminar tratamento de dados do formulário
-    //req.
+    
+    req.assert('usuario', 'Usuário deve ser informado').notEmpty();
+    req.assert('senha', 'Senha deve ser informada').notEmpty();
+    
+    var erros = req.validationErrors();
+
+    if (erros) {
+        res.render("seguranca/login",{validacao: erros});
+        return;
+    }
+
+    res.send('tudo ok!!!');
 }
